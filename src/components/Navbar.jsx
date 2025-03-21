4bc9c460a6210d43654f134a4fe628ec68e88fab
uzwal01 <uzwalduwal07@gmail.com>
2025-03-21 13:58:08 +0545

    minor change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ cartCount, setIsCartOpen }) => {
       <div>
         <nav className="p-4 bg-gray-800 text-white flex justify-between ">
           <h1 className="text-xl">E-commerce</h1>
-          <button onClick={() => setIsCartOpen(true)} className="relative bg-blue-500 rounded-lg px-2 py-1 hover:bg -gray-500 cursor-pointer delay-75 ">
+          <button onClick={() => setIsCartOpen(true)} className="relative bg-blue-500 rounded-lg px-2 py-1 hover:bg-gray-500 cursor-pointer delay-75 ">
              Cart <span className="text-red-400">({cartCount})</span>
           </button>
         </nav>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -14,7 +14,7 @@ const Productlist = ({ products, addToCart }) => {
               <p>${product.price}</p>
               <button
                 onClick={() => addToCart(product)}
-                className="bg-blue-500 text-white px-4 py-2 mt-2"
+                className="bg-blue-500 text-white px-4 py-2 mt-2 rounded-lg hover:bg-gray-500 cursor-pointer delay-75"
               >
                 Add To Cart
               </button>
